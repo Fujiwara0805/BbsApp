a2b49efb028e5d6089002e1166a8768504071b64
Fujiwara0805 <tiki4091@gmail.com>
2024-02-27 21:46:28 +0900

    ♻️ BBSCardList.tsxからBBSCard.tsxへpropsの受け渡し

--- src/components/layouts/BBSCardList/BBSCardList.tsx
+++ src/components/layouts/BBSCardList/BBSCardList.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import BBSCard from "../BBSCard/BBSCard";
+import { BBSData } from "@/types/types";
+interface BbsAllDataProps {
+  BbsAllData: BBSData[];
+}
 
-const BBSCardList = () => {
+const BBSCardList = ({ BbsAllData }: BbsAllDataProps) => {
   return (
     <div className=" grid lg: grid-cols-3 px-4 py-4 gap-4 ">
-      <BBSCard />
-      <BBSCard />
-      <BBSCard />
+      {BbsAllData.map((bbsCard: BBSData) => {
+        return <BBSCard key={bbsCard.id} bbsCard={bbsCard} />;
+      })}
     </div>
   );
 };
