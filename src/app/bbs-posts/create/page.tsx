fc373ac89cf14dc9b7ba5376bde126605ce25c32
Fujiwara0805 <tiki4091@gmail.com>
2024-02-29 08:18:11 +0900

    ✨ 掲示板投稿用画面の実装

--- src/app/bbs-posts/create/page.tsx
+++ src/app/bbs-posts/create/page.tsx
@@ -1,7 +1,115 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Textarea } from "@/components/ui/textarea";
+import { useRouter } from "next/navigation";
+
+/* バリデーション設計 */
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "ユーザー名は2文字以上で入力してください" }),
+  title: z
+    .string()
+    .min(2, { message: "タイトルは2文字以上で入力してください" }),
+  content: z
+    .string()
+    .min(10, { message: "本文は10文字以上で入力してください" })
+    .max(140, { message: "本文は140文字以内で入力してください" }),
+});
 
 const CreateBbs = () => {
-  return <div>CreateBbs</div>;
+  const router = useRouter();
+  /* FORM型定義 */
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      title: "",
+      content: "",
+    },
+  });
+
+  /* 掲示板投稿処理 */
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    const { username, title, content } = value;
+    try {
+      const res = await fetch("http://localhost:3000/api/post", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, title, content }),
+      });
+      console.log(res);
+      router.push("/");
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className=" py-4 px-4">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-3 w-1/2 px-7"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>ユーザー名</FormLabel>
+                <FormControl>
+                  <Input placeholder="ユーザー名" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>タイトル</FormLabel>
+                <FormControl>
+                  <Input placeholder="タイトル" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>コンテンツ</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="コンテンツ" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default CreateBbs;
