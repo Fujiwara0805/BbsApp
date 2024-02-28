63e709ef843ec565bc94a2310939835d4dacdb05
Fujiwara0805 <tiki4091@gmail.com>
2024-02-29 08:17:07 +0900

    👔 掲示板投稿用のAPI を作成

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -6,3 +6,16 @@ export const GET = async (req: Request, res: NextResponse) => {
   const posts = await prisma.post.findMany();
   return NextResponse.json(posts);
 };
+
+/* 掲示板投稿処理API */
+export const POST = async (req: Request, res: NextResponse) => {
+  const { username, title, content } = await req.json();
+  const posts = await prisma.post.create({
+    data: {
+      username,
+      title,
+      content,
+    },
+  });
+  return NextResponse.json(posts);
+};
